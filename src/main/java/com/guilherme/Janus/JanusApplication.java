646ba6b83a36f3cd59860b4f60b09b28030c88a4
Guilherme Sales <guilhermedesaales@gmail.com>
2025-08-04 17:21:23 -0300

    Iniciação do Projeto Janus

---
+++ src/main/java/com/guilherme/Janus/JanusApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.Janus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JanusApplication.class, args);
+	}
+
+}
